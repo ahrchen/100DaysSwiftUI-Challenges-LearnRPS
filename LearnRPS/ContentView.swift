0b9f8b75f4847aeb212262d69019be4b438cf721
Raymond Chen <ahrchen@gmail.com>
2022-02-09 20:38:14 -0800

    Initial Commit

---
+++ LearnRPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnRPS
+//
+//  Created by Raymond Chen on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnRPS/LearnRPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnRPSApp.swift
+//  LearnRPS
+//
+//  Created by Raymond Chen on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnRPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

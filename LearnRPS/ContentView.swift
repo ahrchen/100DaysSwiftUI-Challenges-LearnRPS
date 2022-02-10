f0c5af80856cb271bbc8aae60e4a00ac221e6c29
Raymond Chen <ahrchen@gmail.com>
2022-02-09 20:51:25 -0800

    Setup game variables

--- LearnRPS/ContentView.swift
+++ LearnRPS/ContentView.swift
@@ -7,10 +7,31 @@
 
 import SwiftUI
 
+enum Moves: CaseIterable {
+    case Rock
+    case Paper
+    case Scissor
+}
+
 struct ContentView: View {
+    let winState: [Moves: Moves] = [
+        .Rock: .Scissor,
+        .Paper : .Rock,
+        .Scissor : .Paper
+    ]
+    
+    @State var playerMove: Moves = .Rock
+    @State var cpuMove: Moves = .Paper
+    @State var shouldWin: Bool = false
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+    }
+    
+    func askQuestion() {
+        cpuMove = Moves.allCases.randomElement()!
+        shouldWin = Bool.random()
     }
 }
 
